560ab37ee9c432534c2bbf1a43582313b7a88044
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-02 19:39:05 +0530

    updated folder

---
+++ MAUI_CONTROLS/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using MAUI_CONTROLS.Pages;
+
+namespace MAUI_CONTROLS {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+
+            MainPage = new CommonControls();
+        }
+    }
+}
---
+++ MAUI_CONTROLS/Pages/CommonControls.xaml.cs
@@ -0,0 +1,10 @@
+namespace MAUI_CONTROLS.Pages;
+
+public partial class CommonControls : ContentPage
+{
+	public CommonControls()
+	{
+		InitializeComponent();
+	}
+
+}
